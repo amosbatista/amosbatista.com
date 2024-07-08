d267de5f06f4d1b0f48581cf836342705534c47d
Amós Batista <amos.silva@gmail.com>
2024-07-08 11:58:38 -0300

    feat: jest test

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/**/*.spec.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  coverageDirectory: './coverage',
+  globals: { 'ts-jest': { diagnostics: false } },
+  transform: {},
+};
