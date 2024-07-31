d323cc56d2aa8419a2586b4e5471b4250cb457d2
Amós Batista <amos.silva@gmail.com>
2024-07-31 18:15:23 -0300

    feat: square generator

---
+++ pages/squarer.vue
@@ -0,0 +1,13 @@
+<script setup>
+  
+
+  const theArray = useState('theArray', () => ([1, 2, 3, 4, 5]));
+</script>
+
+<template>
+  <div class="area">
+    <span v-for="el in theArray">
+      {{ el }}
+    </span>
+  </div>
+</template>
