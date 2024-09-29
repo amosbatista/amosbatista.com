85e6173719c240c3d73beba06c681c922f288f7c
Amós Batista <amos.silva@gmail.com>
2024-09-29 16:42:35 -0300

    feat: kinktober 2024

---
+++ components/kinktoberItem.vue
@@ -0,0 +1,20 @@
+<script setup>
+  const { itemName } = defineProps(['itemName']);
+</script>
+<template>
+  <div class="item">
+    {{  itemName  }}
+  </div>
+</template>
+<style>
+  .item {
+    border: 1px solid rgb(219, 56, 159);
+    padding: 10px 15px;
+    margin: 0 5px 5px;
+    border-radius: 3px;
+    background-color: rgb(219, 56, 159);
+    color: white;
+    font-size: 150%;
+    font-weight: bold;
+  }
+</style>
---
+++ pages/kinktober2024/index.vue
@@ -0,0 +1,53 @@
+<script setup>
+  const list = [
+    'manso',
+    'câmera',
+    'frio',
+    'veia',
+    'friso',
+    'amassar',
+    'culpado',
+    'erguer',
+    'asas',
+    'brilho',
+    'engasgar',
+    'canalha',
+    'sabonete',
+    'bota',
+    'caça',
+    'umbigo',
+    'formal',
+    'vermelho',
+    'demônio',
+    'passade',
+    'banho',
+    'decente',
+    'praia',
+    'instigar',
+    'palma',
+    'noite',
+    'colega',
+    'faca',
+    'aconchegar',
+    'despudorar'
+  ]
+</script>
+<template>
+  <h1>kinktober 2024</h1>
+
+  <div class="the-zone">
+    <kinktober-item
+      v-for="(palavra, index) in list" :itemName="(index + 1) + ' - ' + palavra">
+    </kinktober-item>
+  </div>
+</template>
+
+<style>
+  .the-zone {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+
+  }
+</style>
