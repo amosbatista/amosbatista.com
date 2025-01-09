68f69b90596668c04873f3fc73a9858002d65dda
Amós Batista <amos.silva@gmail.com>
2025-01-09 12:40:30 -0300

    chore: update home

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-    <h1>amosbatista.com</h1>
-    <p>em construção</p>
+  <div class="main">
+    <div class="text-center">
+      <h1>amosbatista.com</h1>
+      <RouterLink to="/vis"><p>Generator - vis</p></RouterLink>
+      <RouterLink to="/sound"><p>Sound Generator - Game of Life</p></RouterLink>
+      <a :href="'mailto:amos.silva@gmail.com'"><p>amos.silva@gmail.com</p></a>
+    </div>
   </div>
 </template>
+<style>
+  .main{
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+    height: 90vh;
+  }
+  .text-center {
+    text-align: center;
+  }
+</style>
